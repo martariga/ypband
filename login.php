3a72a31c91c4cc8741df94be2e3ebac446486a72
martariga <marta.rigamonti97@gmail.com>
2016-01-25 13:07:51 +0100

    Update login.php

--- login.php
+++ login.php
@@ -9,14 +9,14 @@ if(!isset($_SESSION))
 if(isset($_SESSION["username"]))
  {
  	echo "<script>window.location.href='index.html';</script>";
-    echo "<script>alert('sei già loggato');</script>"
+    echo "<script>alert('sei già loggato');</script>";
  	return;
  }
  
 if(isset($_POST['nomeUtente']) && isset($_POST['password']))
 {
 	$user=$dbhandle->real_escape_string($_POST['nomeUtente']);
-    $pass=$dbhandle->real_escape_string($_POST['password']);
+    $pass=md5($_POST['password']);
     
 	$query = "select * from users where username='$user' and password='$pass'";
 	$result = $dbhandle->query($query) or die ("Hai sbagliato la query: ".$query);
@@ -24,7 +24,6 @@ if(isset($_POST['nomeUtente']) && isset($_POST['password']))
     if($numRows==1)
     {
     	echo "sei loggato";
-		echo "ciaooooo";
         $_SESSION['username'] = $user;
         $_SESSION['password'] = $pass;
         echo "<a href = \"esci.php\">esci</a>";
